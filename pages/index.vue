5b96d1fa0c6e07768dd76bb71891d8d2720e603b
Michal Varga <vargamisko@gmail.com>
2021-05-01 12:57:00 +0200

    changed chipNumber to string

--- pages/index.vue
+++ pages/index.vue
@@ -94,7 +94,7 @@ export default {
   data() {
     return {
       visiblestep: 0,
-      chipNumber: ,
+      chipNumber: "",
       isicCheckLetter: "",
       covidpass: "",
       adultstudent: false,
