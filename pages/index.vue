bdfd4631580e7503922116228d0e2d9e519931c3
Michal Varga <vargamisko@gmail.com>
2021-05-03 01:44:58 +0200

    next version of NFC implementation

--- pages/index.vue
+++ pages/index.vue
@@ -236,7 +236,7 @@
 </defs>
 </svg>
       <h2 class="text-center font-bold mt-2">Na vrátnici sa prosím preukáž svojou ISIC kartou.</h2>
-      <p class="home" @click="tostart(); resetIsicInput(); existingcookiescheck();"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <p class="home" @click="tostart(); resetIsicInput(); existingcookiescheck(); forgetScannedUID();"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
       </svg>Na začiatok</p>
     </div>
@@ -515,6 +515,9 @@ export default {
         this.nfcAble = true;
       }
     },
+    forgetScannedUID(){
+      this.scannedUID = "";
+    },
     async nfcRead() {
   try {
     const ndef = new NDEFReader();
@@ -543,7 +546,7 @@ export default {
   },
   beforeMount(){
     this.existingcookiescheck();
-    //this.nfcCheck();
+    this.nfcCheck();
   },
   mounted () {
    
