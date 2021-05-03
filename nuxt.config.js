8424ab06409e72e5950d71ef43a9046f33765b29
Michal Varga <vargamisko@gmail.com>
2021-05-03 16:18:27 +0200

    changed render config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
-  target: "server",
+  target: 'static', // default is 'server'
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
