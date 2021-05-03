0337a1490c2cd20257cb27db2cd6bc6c6a47fe6f
Michal Varga <vargamisko@gmail.com>
2021-05-03 20:52:47 +0200

    added theme-color meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { theme_color: '#009EE2' },
+      { name: 'theme-color', content: '#009EE2' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
